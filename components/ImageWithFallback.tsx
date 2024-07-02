b307b577e612265880edf3940479ea3fca50ded1
itsjagdeep <jagdeep1834@gmail.com>
2024-07-02 01:44:32 -0400

    fix eslint probs

--- components/ImageWithFallback.tsx
+++ components/ImageWithFallback.tsx
@@ -3,19 +3,32 @@
 import Image from "next/image";
 import React, { useState } from "react";
 
-const ImageWithFallback = (props: {
+interface ImageWithFallbackProps {
   src: string;
   alt: string;
-  className: string;
-}) => {
-  const [imgSrc, setImgSrc] = useState(props.src);
+  className?: string;
+  width?: number;
+  height?: number;
+}
+
+const ImageWithFallback = ({ 
+  src, 
+  alt, 
+  className, 
+  width = 300, 
+  height = 450,
+  ...rest 
+}: ImageWithFallbackProps) => {
+  const [imgSrc, setImgSrc] = useState(src);
 
   return (
     <Image
-      width={300}
-      height={450}
-      {...props}
       src={imgSrc}
+      alt={alt}
+      className={className}
+      width={width}
+      height={height}
+      {...rest}
       onError={() => {
         setImgSrc(
           "https://davidkoepp.com/wp-content/themes/blankslate/images/Movie%20Placeholder.jpg"
